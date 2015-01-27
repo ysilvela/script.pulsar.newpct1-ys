03e7f711ab003d22d0a8e4dd9395582d4b6bd2b2
ysilvela <ysilvela@gmail.com>
2015-01-27 23:28:24 +0100

    Ultimos cambios adaptando a newpct1

--- main.py
+++ main.py
@@ -18,16 +18,35 @@ def extract_torrents(data, query):
         filters.information()  # Pintamos las opciones de filtrado en el log
         data = common.clean_html(data) # Elimina los comentarios que haya ('<!--(.*?)-->')
         cont = 0
+        last_item = ""
         pattern = r'<a\shref=[\'"]?([^\'" >]+%s)' % query
-        for cm, (torrent, name) in enumerate(re.findall(pattern, data)):
-            torrent = settings.url + '/get-torrent/' + torrent  # create torrent to send Pulsar
-            if filters.verify(name, None):
-                    yield {"name": name + ' - ' + settings.name_provider, "uri": torrent}  # return le torrent
+        for cm,item in enumerate(re.findall(pattern, data)): #http://www.newpct1.com/descarga-torrent/pelicula/interstellar/
+            if last_item != item:
+                next_url = item.replace(".com/",".com/descarga-torrent/") + "/"
+                browser.open(next_url)
+                print "Next Url : " + next_url
+                data_next = browser.content
+                pattern_next = '<a href="([^"]+)" title="[^"]+" class="btn-torrent" target="_blank">'
+                # Con el patron anterior obtengo <a href="http://tumejorjuego.com/download/index.php?link=descargar-torrent/058310_yo-frankenstein-blurayrip-ac3-51.html" title="Descargar torrent de Yo Frankenstein " class="btn-torrent" target="_blank">Descarga tu Archivo torrent!</a>
+
+                link =re.findall(pattern_next,data_next)
+                provider.log.info("Link : " + link[0])
+                partes = link[0].split("/")
+                cadena = partes[ len(partes)-1 ]
+                torrent = cadena.split("_")
+                provider.log.info("Torrent : " + torrent[0])
+                cadena = torrent[1].split(".")
+                titulo = cadena[0]
+                provider.log.info("Titulo : " + titulo)
+        
+                if filters.verify(titulo, None):
+                    yield {"name": titulo + ' - ' + settings.name_provider, "uri": link[0]}  # devuelve el torrent
                     cont += 1
-            else:
-                provider.log.warning(filters.reason)
-            if cont == settings.max_magnets:  # limit magnets
-                break
+                else:
+                    provider.log.warning(filters.reason)
+                if cont == settings.max_magnets:  # limit magnets
+                    break  
+            last_item = item
         provider.log.info('>>>>>>' + str(cont) + ' torrents sent to Pulsar<<<<<<<')
     except:
         provider.log.error('>>>>>>>ERROR parsing data from newpct1<<<<<<<')
@@ -40,7 +59,7 @@ def search(query):
     query += ' ' + settings.extra
     if settings.time_noti > 0: provider.notify(message="Searching: " + query.encode('utf-8','ignore').title() + '...', header=None, time=settings.time_noti, image=settings.icon)
     query = provider.quote_plus(query.lstrip())
-    url_search = "%s/buscar/index.php?page=buscar&q=%s" % (settings.url,query)
+    url_search = "%s/buscar/index.php?page=buscar&q=%s&ordenar=Nombre&inon=Ascendente" % (settings.url,query)
     provider.log.info(url_search)
     if browser.open(url_search):
         results = extract_torrents(browser.content, query)
