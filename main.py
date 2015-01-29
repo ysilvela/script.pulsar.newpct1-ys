6c8be50160d3586139ae41d2e85584ed9fb63f5b
ysilvela <ysilvela@gmail.com>
2015-01-29 09:55:06 +0100

    ultimos cambios

--- main.py
+++ main.py
@@ -23,23 +23,36 @@ def extract_torrents(data, query):
         last_item = ''
         pattern = r'<a\shref=[\'"]?([^\'" >]+%s.*?").*?<span>(.*?)</span>.*?<span>(.*?[GB MB])</span>' % query_re
         provider.log.info('Patron : ' + pattern)
-        if data=='': provider.log.info('Error. No vienen datos ' )
-        for cm,(item,fecha,tam) in enumerate(re.findall(pattern, data)): #http://www.newpct1.com/descarga-torrent/pelicula/interstellar/
-            provider.log.info('Item url : ' + item) 
-            if last_item != item:
-                next_url = item.replace(".com/",".com/descarga-torrent/") + "/"
+        datos_lista = data
+        pagina = 0
+        while True:
+          pagina = pagina + 1
+          if datos_lista=='': provider.log.info('Error. No vienen datos ' )
+          for cm,(item,fecha,tam) in enumerate(re.findall(pattern, datos_lista)): #http://www.newpct1.com/descarga-torrent/peliculas/carretera-perdida-1997--en--blurayrip-ac3-5-1--peliculas
+            nombre_largo = item.split("/")[4]
+            provider.log.info("Nombre largo: " + nombre_largo)
+            if last_item != item and nombre_largo == query:
+                provider.log.info('Item url : ' + item) 
+                next_url = item.replace(".com/",".com/descarga-torrent/")
+                next_url = next_url.replace('"','')
+                print next_url
+                print next_url.split("/")
                 nombre = next_url.split("/")[4]
                 browser.open(next_url)
                 provider.log.info('Next Url : ' + next_url)
+                print browser.status
                 data_next = browser.content
                 pattern_next = '<a href="([^"]+)" title="[^"]+" class="btn-torrent" target="_blank">'
                 # Con el patron anterior obtengo <a href="http://tumejorjuego.com/download/index.php?link=descargar-torrent/058310_yo-frankenstein-blurayrip-ac3-51.html" title="Descargar torrent de Yo Frankenstein " class="btn-torrent" target="_blank">Descarga tu Archivo torrent!</a>
                 
                 link =re.findall(pattern_next,data_next)
-                provider.log.info('Link : ' + link[0])
+                link_url = link[0]
+                if link_url[len(link_url)] <> "/": link_url = link_url + "/"
+                provider.log.info('Link : ' + link_url)
                 partes = link[0].split("/")
-                cadena = partes[ len(partes)-1 ]
+                cadena = partes[ len(partes)-2 ]
                 torrent = cadena.split("_")
+                print torrent
                 provider.log.info('Torrent : ' + torrent[0])
                 cadena = torrent[1].split(".")
                 titulo = cadena[0]
@@ -54,8 +67,27 @@ def extract_torrents(data, query):
                 if cont == settings.max_magnets:  # limit magnets
                     break  
             last_item = item
+          # Elimina los comentarios que haya ('<!--(.*?)-->')
+          if pagina == 2: break
+          if "Next" in datos_lista:
+            provider.log.info('Hay mas paginas. Paginamos')
+            url_next_page  = re.findall('Next.*?<a href=(.*?)>',datos_lista)[0]
+            url_next_page = url_next_page.replace('"',"")
+            provider.log.info("Siguiente enlace de paginacion : " + url_next_page)
+            browser.content = ''
+            browser.open(url_next_page)
+            print browser.status
+            if "Next" in browser.content: provider.log.info('<<<<<<<<<<<<ERORRRRR>>>>>>>>>>>>>>>>>>>>>>')
+            datos_lista = ''
+            datos_lista = browser.content
+            datos_lista = common.clean_html(datos_lista)
+            # print datos_lista
+          else:
+             provider.log.info('<<<<<<<<<<<<<<<<<No hay mas paginas. >>>>>>>>>>>><')
+             break
         provider.log.info('>>>>>>' + str(cont) + ' torrents sent to Pulsar<<<<<<<')
     except:
+        print "Unexpected error:", sys.exc_info()
         provider.log.error('>>>>>>> ERROR parsing data from newpct1<<<<<<<')
         provider.notify(message='ERROR parsing data', header=None, time=5000, image=settings.icon)
 
